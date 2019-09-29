e27e85670f9fa3794e37f4bc317b0d141bbc9361
David Poslt <david.poslt@gmail.com>
2019-09-29 17:06:07 +0200

    add mouse position

--- generator.py
+++ generator.py
@@ -1,19 +1,24 @@
-#!/etc/bin/env python
+#!/usr/bin/env python3
 
 __author__ = 'David Poslt'
 
 import random
+import pyautogui
 
 charts = 'abcdefghijklmnopqrstuvwxyz1234567890#@!'
-'''
-delka = 0
-#retezec = []
+count = 12
+position = []
 
-while delka <=11:
-    delka +=1
-    retezec.append(random.choice(charts))
-'''
+while count != 0:
+    mouse = pyautogui.position()
+    x = mouse[0]
+    y = mouse[1]
+    position.append(x)
+    position.append(y)
+    count -=1
 
+
+print(position)
 retezec = random.sample(charts,12)
 
 print(''.join(retezec))
