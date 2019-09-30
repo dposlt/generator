930ce0c06532e9eda296538032f9d9bfd7316e6a
David Poslt (MONETA) <david.poslt@moneta.cz>
2019-09-30 11:51:02 +0200

    add mouse positions

--- generator.py
+++ generator.py
@@ -6,19 +6,27 @@ import random
 import pyautogui
 
 charts = 'abcdefghijklmnopqrstuvwxyz1234567890#@!'
-count = 12
+
+
 position = []
+def mousePosition():
+    import sys
+    count = 16
 
-while count != 0:
-    mouse = pyautogui.position()
-    x = mouse[0]
-    y = mouse[1]
-    position.append(x)
-    position.append(y)
-    count -=1
+    print('Move mouse to quit.')
 
+    while len(position) != count:
+        x, y = pyautogui.position()
+        positionX = str(x).rjust(4)
+        positionY = str(y).rjust(4)
+        if positionX not in position and positionY not in position:
+            position.append(positionX)
+            position.append(positionY)
+            #print(position)
+
+
+
+    return position
+mousePosition()
 
 print(position)
-retezec = random.sample(charts,12)
-
-print(''.join(retezec))
