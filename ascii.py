c3045dee937437aa4af80c59543fcfdb2293e720
David Poslt (MONETA) <david.poslt@moneta.cz>
2019-09-30 14:38:15 +0200

    add ascii tab

---
+++ ascii.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+__author__= 'David Poslt'
+
+class ASCII:
+
+
+    def showASCITable():
+        character = {}
+        for num in range(33,127):
+            charts = chr(num)
+            character[num] = charts
+            #print('{} : {}'.format(num, charts))
+        return character
+
+
+
+getChar = ASCII.showASCITable()
+
+znak = 88
+if znak in getChar:
+    print(getChar[znak])
